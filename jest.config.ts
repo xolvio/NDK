a495aa90cd0cfa4e4faf9ac0cd90444781d09f6f
Sam Hatoum <samhatoum@users.noreply.github.com>
2023-05-30 22:11:58 -0400

    Sets coverage thresholds

--- jest.config.ts
+++ jest.config.ts
@@ -17,6 +17,14 @@ const config: Config = {
       },
     ],
   },
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
 
 export default config;
